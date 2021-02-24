fb8c528acb62e74179bfcef22fb8147e1bccc625
ruirchik@mail.ru <ruirchik@mail.ru>
2021-02-24 12:43:49 +0300

    свайпер

---
+++ source/js/modules/about-cert.js
@@ -0,0 +1,8 @@
+console.log('123')
+const aboutCertSlider = new Swiper('.about-cert__list', {
+    slidesPerView: 6,
+    // navigation: {
+    // // nextEl: '.art__next',
+    // // prevEl: '.art__prev',
+    // },
+});
--- source/js/vendor.js
+++ source/js/vendor.js
@@ -1,2 +1,4 @@
 //= vendor/jquery.3.min.js
 //= vendor/inputmask.js
+//= vendor/jquery.fancybox.js
+//= vendor/swiper.js
