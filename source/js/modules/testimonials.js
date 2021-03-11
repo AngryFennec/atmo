245ec169189fafc91f3bb7df3e31208f73228702
ruirchik@mail.ru <ruirchik@mail.ru>
2021-03-11 09:49:49 +0300

    попап с отзывами

--- source/js/modules/testimonials.js
+++ source/js/modules/testimonials.js
@@ -1,18 +1,22 @@
-const aboutCertSlider = new Swiper('.video-testi__slider', {
-    slidesPerView: 1,
-    loop: true,
-    navigation: {
-        nextEl: '.video-testi__next',
-        prevEl: '.video-testi__prev',
-    },
-});
+if ($('.video-testi__slider')[0]) {
+    const aboutCertSlider = new Swiper('.video-testi__slider', {
+        slidesPerView: 1,
+        loop: true,
+        navigation: {
+            nextEl: '.video-testi__next',
+            prevEl: '.video-testi__prev',
+        },
+    });
+}
 
-const promoCertSlider = new Swiper('.testi-promo__slider', {
-    slidesPerView: 1,
-    effect: 'fade',
-    loop: true,
-    navigation: {
-        nextEl: '.testi-promo__next',
-        prevEl: '.testi-promo__prev',
-    },
-});
+if ($('.testi-promo__slider')[0]) {
+    const promoCertSlider = new Swiper('.testi-promo__slider', {
+        slidesPerView: 1,
+        effect: 'fade',
+        loop: true,
+        navigation: {
+            nextEl: '.testi-promo__next',
+            prevEl: '.testi-promo__prev',
+        },
+    });
+}
