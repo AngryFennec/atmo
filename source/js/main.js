55c3b7530b2c2ff12b092bf9e39dc0caa3e8d76e
Angry Fennec <shellfix@gmail.com>
2021-03-16 18:38:39 +0300

    Добавляет страницу с ценами

--- source/js/main.js
+++ source/js/main.js
@@ -1,8 +1,8 @@
-//= modules/title-scroll.js
 //= modules/modal.js
 //= modules/header-menu.js
 //= modules/about-cert.js
 //= modules/phone-masks.js
 //= modules/testimonials.js
 //= modules/popup.js
+//= modules/title-scroll.js
 
--- source/js/modules/title-scroll.js
+++ source/js/modules/title-scroll.js
@@ -1,43 +1,37 @@
 (function () {
-  // var a = document.querySelector('.js-scrolled-title'), b = null, P = 0;
-  // window.addEventListener('scroll', Ascroll, false);
-  // document.body.addEventListener('scroll', Ascroll, false);
+  function Ascroll() {
+    if (document.documentElement.clientWidth > 768) {
+      var P = document.querySelector('.header').getBoundingClientRect().height;
+      containers.forEach((item, j) => {
+        if (containers[j].getBoundingClientRect().top - P <= 0 && containers[j].getBoundingClientRect().bottom - P - titles[j].getBoundingClientRect().height  >= 0) {
+          titles[j].style.position = 'fixed';
+          titles[j].style.top = P + 'px';
+        } else {
+          if (containers[j].getBoundingClientRect().bottom - P  - titles[j].getBoundingClientRect().height < 0) {
+            titles[j].style.position = 'absolute';
+            titles[j].style.top = `calc(100% - ${titles[j].getBoundingClientRect().height}px)`;
+          } else {
+            titles[j].style.position = 'absolute';
+            titles[j].style.top = '0';
+          }
+        }
+      });
+    } else {
+      titles.forEach((item) => {
+        item.style.position = 'relative';
+        item.style.top = '0';
+      });
 
-  // function Ascroll() {
-  //   if (b == null) {
-  //     var Sa = getComputedStyle(a, ''), s = '';
-  //     for (var i = 0; i < Sa.length; i++) {
-  //       if (Sa[i].indexOf('overflow') == 0 || Sa[i].indexOf('padding') == 0 || Sa[i].indexOf('border') == 0 || Sa[i].indexOf('outline') == 0 || Sa[i].indexOf('box-shadow') == 0 || Sa[i].indexOf('background') == 0) {
-  //         s += Sa[i] + ': ' + Sa.getPropertyValue(Sa[i]) + '; ';
-  //       }
-  //     }
-  //     b = document.createElement('div');
-  //     b.style.cssText = s + ' box-sizing: border-box; width: ' + a.offsetWidth + 'px;';
-  //     a.insertBefore(b, a.firstChild);
-  //     var l = a.childNodes.length;
-  //     for (var i = 1; i < l; i++) {
-  //       b.appendChild(a.childNodes[1]);
-  //     }
-  //     a.style.height = b.getBoundingClientRect().height + 'px';
-  //     a.style.padding = '0';
-  //     a.style.border = '0';
-  //   }
-  //   var Ra = a.getBoundingClientRect(),
-  //     R = Math.round(Ra.top + b.getBoundingClientRect().height - document.querySelector('.js-scrolled-container').getBoundingClientRect().bottom);  // селектор блока, при достижении нижнего края которого нужно открепить прилипающий элемент
-  //   if ((Ra.top - P) <= 0) {
-  //     if ((Ra.top - P) <= R) {
-  //       b.className = 'stop';
-  //       b.style.top = -R + 'px';
-  //     } else {
-  //       b.className = 'sticky';
-  //       b.style.top = P + 'px';
-  //     }
-  //   } else {
-  //     b.className = '';
-  //     b.style.top = '';
-  //   }
-  //   window.addEventListener('resize', function () {
-  //     a.children[0].style.width = getComputedStyle(a, '').width;
-  //   }, false);
-  // }
-})();
+    }
+  }
+
+  var titles = Array.from(document.querySelectorAll('.js-scrolled-title'));
+  var containers = Array.from(document.querySelectorAll('.js-scrolled-container'));
+
+  if (titles && titles.length > 0 && containers && containers.length > 0) {
+    window.addEventListener('resize', Ascroll);
+    window.addEventListener('scroll', Ascroll, false);
+    document.body.addEventListener('scroll', Ascroll, false);
+  }
+})
+();
