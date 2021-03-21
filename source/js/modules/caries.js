5e16d5f3b2596118fcef393c145c0385da67d61e
Angry Fennec <shellfix@gmail.com>
2021-03-21 18:55:10 +0300

    final

--- source/js/modules/caries.js
+++ source/js/modules/caries.js
@@ -21,3 +21,53 @@ if ($('.caries-ba__swiper')[0]) {
     },
   });
 }
+
+var cariesProcessSwiper = document.querySelector('.caries-process__slider');
+if (cariesProcessSwiper) {
+  var cariesProcess = new Swiper('.caries-process__slider', {
+    navigation: {
+      nextEl: '.caries-process__next',
+      prevEl: '.caries-process__prev',
+    },
+  });
+}
+
+var cariesAboutSwiper = document.querySelector('.caries-about-top');
+if (cariesAboutSwiper) {
+
+  var cariesThumbs = new Swiper('.caries-about-thumbs', {
+    spaceBetween: 16,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
+    slidesPerView: 4,
+  });
+  var cariesTop = new Swiper('.caries-about-top', {
+    init: false,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: '.caries-about__next',
+      prevEl: '.caries-about__prev',
+    },
+    thumbs: {
+      swiper: cariesThumbs
+    },
+  });
+
+
+
+  cariesTop.on('activeIndexChange', function () {
+    var cariesCurrent = document.querySelector('.caries-about__current');
+    var cariesAll = document.querySelector('.caries-about__all');
+    cariesAll.textContent = cariesTop.slides.length;
+    cariesCurrent.textContent = cariesTop.activeIndex+1;
+  });
+
+  cariesTop.on('init', function () {
+    var cariesCurrent = document.querySelector('.caries-about__current');
+    var cariesAll = document.querySelector('.caries-about__all');
+    cariesAll.textContent = cariesTop.slides.length;
+    cariesCurrent.textContent = cariesTop.activeIndex+1;
+  });
+
+  cariesTop.init();
+}
